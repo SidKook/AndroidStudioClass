83c6d70ed316181abbb1a70d14d69a211c5bfb23
SidKook <110208722+SidKook@users.noreply.github.com>
2024-04-05 19:28:57 -0500

    Learned Log & TextView

--- app/src/main/java/com/company/androidstudioclass/MainActivity.java
+++ app/src/main/java/com/company/androidstudioclass/MainActivity.java
@@ -2,13 +2,52 @@ package com.company.androidstudioclass;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String firstName = "Sid";
+
+        // Displaying Logs
+        Log.d(TAG,"My Name is " + firstName);
+        // Log.e(TAG,"My Name is sid");
+
+        String lastName = "Kukert";
+
+        Log.d(TAG,"My full name is " + firstName + " " + lastName);
+
+        Integer myFavoriteNumber = 34;
+
+        Log.d(TAG,"My favorite number is " + myFavoriteNumber);
+
+        Integer mySecondNumber = 17;
+
+        Integer bothNumbers = myFavoriteNumber + mySecondNumber;
+
+        Log.d(TAG,"Both numbers = " + bothNumbers);
+
+        String myThirdNumber = "15";
+
+        myFavoriteNumber = myFavoriteNumber + mySecondNumber + Integer.valueOf(myThirdNumber);
+
+        Log.d(TAG,"My new favorite number = " + myFavoriteNumber);
+
+
+        TextView myNameIsTV = findViewById(R.id.MyNameTextView);
+
+        myNameIsTV.setText("I like burgers");
+        myNameIsTV.setTextColor(Color.RED);
+        myNameIsTV.setTextSize(50);
+
+
     }
 }
